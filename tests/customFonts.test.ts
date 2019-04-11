c618cb1131cbe123a623946798fb3d42cf147181
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-04-11 18:36:29 +0300

    Fixed customFonts test

--- tests/customFonts.test.ts
+++ tests/customFonts.test.ts
@@ -1,20 +1,16 @@
 import * as jsPDF from "jspdf";
 import addCustomFonts from "../src/customFonts";
+addCustomFonts(jsPDF);
+let doc = new jsPDF();
 
 test("add segoe font  to jsPDf", () => {
-  addCustomFonts(jsPDF);
-  let doc = new jsPDF();
   expect(doc.getFontList()["segoe"]).toBeDefined();
 });
 
 test("add segoe font (normal) to jsPDf", () => {
-  addCustomFonts(jsPDF);
-  let doc = new jsPDF();
   expect(doc.getFontList()["segoe"].includes("normal")).toBe(true);
 });
 
 test("add segoe font (bold) to jsPDf", () => {
-  addCustomFonts(jsPDF);
-  let doc = new jsPDF();
   expect(doc.getFontList()["segoe"].includes("bold")).toBe(true);
 });
