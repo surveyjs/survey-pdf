88380847a850b3ee9761d2e01a359d92048d8db1
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-04-22 12:32:18 +0300

    Add checkbox tests

--- tests/checkbox.test.ts
+++ tests/checkbox.test.ts
@@ -1,13 +1,13 @@
 (<any>window)['HTMLCanvasElement'].prototype.getContext = () => {
-  return {};
+	return {};
 };
 
 import { PdfSurvey } from '../src/survey';
-import { CheckBoxQuestion } from '../src/checkbox';
+import { FlatCheckbox } from '../src/flat_layout/flat_checkbox';
 import { TestHelper } from '../src/helper_test';
-let __dummy_cx = new CheckBoxQuestion(null, null);
+let __dummy_flatcheckbox = new FlatCheckbox(null, null);
 
-test.skip('Test has other checkbox', () => {
+test('Test has other checkbox', () => {
 	let json = {
 		questions: [
 			{
@@ -32,133 +32,93 @@ test.skip('Test has other checkbox', () => {
 	expect(internalOtherCheckBox.FT).toBe('/Btn');
 });
 
-test.skip('Test has other split', () => {});
+test.skip('Test has other split', () => { });
 
-// test('Test duplicate value other', () => {
-//   let json = {
-//     questions: [
-//       {
-//         name: 'checkbox',
-//         type: 'checkbox',
-//         choices: ['other'],
-//         hasOther: true
-//       }
-//     ]
-//   };
-//   let survey: JsPdfSurveyModel = new JsPdfSurveyModel(json);
-//   survey.render({
-//     fontSize: 30,
-//     xScale: 0.22,
-//     yScale: 0.36,
-//     margins: {
-//       marginLeft: 10,
-//       marginRight: 10,
-//       marginTop: 10,
-//       marginBot: 10
-//     }
-//   });
-//   let internal = survey.docController.doc.internal;
-//   let internalOtherCheckBoxChoice =
-//     internal.acroformPlugin.acroFormDictionaryRoot.Fields[0];
-//   let internalOtherTextFieldChoice =
-//     internal.acroformPlugin.acroFormDictionaryRoot.Fields[1];
-//   let internalOtherCheckBox =
-//     internal.acroformPlugin.acroFormDictionaryRoot.Fields[2];
-//   let internalOtherTextField =
-//     internal.acroformPlugin.acroFormDictionaryRoot.Fields[3];
-//   expect(internalOtherCheckBoxChoice.FT).toBe('/Btn');
-//   expect(internalOtherTextFieldChoice.FT).toBe('/Tx');
-//   expect(internalOtherCheckBox.FT).toBe('/Btn');
-//   expect(internalOtherTextField.FT).toBe('/Tx');
-// });
-// test('Test all items disabled', () => {
-//   let json = {
-//     questions: [
-//       {
-//         name: 'checkbox',
-//         type: 'checkbox',
-//         choices: ['item1', 'item2', 'item3'],
-//         readOnly: true
-//       }
-//     ]
-//   };
-//   let survey: JsPdfSurveyModel = new JsPdfSurveyModel(json);
-//   survey.render({
-//     fontSize: 30,
-//     xScale: 0.22,
-//     yScale: 0.36,
-//     margins: {
-//       marginLeft: 10,
-//       marginRight: 10,
-//       marginTop: 10,
-//       marginBot: 10
-//     }
-//   });
-//   survey.docController.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
-//     (acroCheckBox: any) => {
-//       expect(acroCheckBox.readOnly).toBe(true);
-//     }
-//   );
-// });
-// test('Test all items enabled', () => {
-//   let json = {
-//     questions: [
-//       {
-//         name: 'checkbox',
-//         type: 'checkbox',
-//         choices: ['item1', 'item2', 'item3'],
-//         readOnly: false
-//       }
-//     ]
-//   };
-//   let survey: JsPdfSurveyModel = new JsPdfSurveyModel(json);
-//   survey.render({
-//     fontSize: 30,
-//     xScale: 0.22,
-//     yScale: 0.36,
-//     margins: {
-//       marginLeft: 10,
-//       marginRight: 10,
-//       marginTop: 10,
-//       marginBot: 10
-//     }
-//   });
-//   survey.docController.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
-//     (acroCheckBox: any) => {
-//       expect(acroCheckBox.readOnly).toBe(false);
-//     }
-//   );
-// });
+test('Test duplicate value other', () => {
+	let json = {
+		questions: [
+			{
+				name: 'checkbox',
+				type: 'checkbox',
+				choices: ['other'],
+				hasOther: true
+			}
+		]
+	};
+	let survey: PdfSurvey = new PdfSurvey(json, TestHelper.defaultOptions);
+	survey.render();
+	let internal = survey.controller.doc.internal;
+	let internalOtherCheckBoxChoice =
+		internal.acroformPlugin.acroFormDictionaryRoot.Fields[0];
+	let internalOtherTextFieldChoice =
+		internal.acroformPlugin.acroFormDictionaryRoot.Fields[1];
+	let internalOtherCheckBox =
+		internal.acroformPlugin.acroFormDictionaryRoot.Fields[2];
+	let internalOtherTextField =
+		internal.acroformPlugin.acroFormDictionaryRoot.Fields[3];
+	expect(internalOtherCheckBoxChoice.FT).toBe('/Btn');
+	expect(internalOtherTextFieldChoice.FT).toBe('/Tx');
+	expect(internalOtherCheckBox.FT).toBe('/Btn');
+	expect(internalOtherTextField.FT).toBe('/Tx');
+});
+test('Test all items disabled', () => {
+	let json = {
+		questions: [
+			{
+				name: 'checkbox',
+				type: 'checkbox',
+				choices: ['item1', 'item2', 'item3'],
+				readOnly: true
+			}
+		]
+	};
+	let survey: PdfSurvey = new PdfSurvey(json, TestHelper.defaultOptions);
+	survey.render();
+	survey.controller.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
+		(acroCheckBox: any) => {
+			expect(acroCheckBox.readOnly).toBe(true);
+		}
+	);
+});
+test('Test all items enabled', () => {
+	let json = {
+		questions: [
+			{
+				name: 'checkbox',
+				type: 'checkbox',
+				choices: ['item1', 'item2', 'item3'],
+				readOnly: false
+			}
+		]
+	};
+	let survey: PdfSurvey = new PdfSurvey(json, TestHelper.defaultOptions);
+	survey.render();
+	survey.controller.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
+		(acroCheckBox: any) => {
+			expect(acroCheckBox.readOnly).toBe(false);
+		}
+	);
+});
 
-// test('Test enable one item', () => {
-//   let json = {
-//     questions: [
-//       {
-//         name: 'checkbox',
-//         type: 'checkbox',
-//         choices: ['item1', 'item2', 'item3'],
-//         choicesEnableIf: '{item} == item2'
-//       }
-//     ]
-//   };
-//   const INDEX_OF_ENABLED_ITEM = 1;
-//   let survey: JsPdfSurveyModel = new JsPdfSurveyModel(json);
-//   survey.render({
-//     fontSize: 30,
-//     xScale: 0.22,
-//     yScale: 0.36,
-//     margins: {
-//       marginLeft: 10,
-//       marginRight: 10,
-//       marginTop: 10,
-//       marginBot: 10
-//     }
-//   });
-//   survey.docController.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
-//     (acroCheckBox: any, index: number) => {
-//       if (index === INDEX_OF_ENABLED_ITEM)
-//         expect(acroCheckBox.readOnly).toBe(false);
-//       else expect(acroCheckBox.readOnly).toBe(true);
-//     }
-//   );
-// });
+test('Test enable one item', () => {
+	let json = {
+		questions: [
+			{
+				name: 'checkbox',
+				type: 'checkbox',
+				choices: ['item1', 'item2', 'item3'],
+				choicesEnableIf: '{item} == item2'
+			}
+		]
+	};
+	const INDEX_OF_ENABLED_ITEM = 1;
+	let survey: PdfSurvey = new PdfSurvey(json, TestHelper.defaultOptions);
+	survey.render();
+	survey.controller.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields.forEach(
+		(acroCheckBox: any, index: number) => {
+			if (index === INDEX_OF_ENABLED_ITEM)
+				expect(acroCheckBox.readOnly).toBe(false);
+			else expect(acroCheckBox.readOnly).toBe(true);
+		}
+	);
+});
