f3d6d80843da565566bb36749485ceb589127c1b
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-04-23 16:58:14 +0300

    Add addBrick method to ComposeBrick

--- src/pdf_render/pdf_compose.ts
+++ src/pdf_render/pdf_compose.ts
@@ -2,23 +2,33 @@ import { IPdfBrick, PdfBrick } from './pdf_brick';
 import { SurveyHelper } from '../helper_survey';
 
 export class ComposeBrick implements IPdfBrick {
-    private pdfBricks: IPdfBrick[];
+    private pdfBricks: IPdfBrick[] = [];
     xLeft: number;
     xRight: number;
     yTop: number;
     yBot: number;
     constructor(...pdfBricks: IPdfBrick[]) {
-        this.pdfBricks = pdfBricks;
+        if (pdfBricks.length != 0) {
+            pdfBricks.push(...pdfBricks);
+            let mergeRect = SurveyHelper.mergeRects(...pdfBricks);
+            this.xLeft = mergeRect.xLeft;
+            this.xRight = mergeRect.xRight;
+            this.yTop = mergeRect.yTop;
+            this.yBot = mergeRect.yBot;
+        }
+    }
+    render(): void {
+        this.pdfBricks.forEach((pdfBrick: IPdfBrick) => {
+            pdfBrick.render();
+        });
+    }
+    addBrick(...pdfBricks: IPdfBrick[]) {
+        this.pdfBricks.push(...pdfBricks);
         let mergeRect = SurveyHelper.mergeRects(...pdfBricks);
         this.xLeft = mergeRect.xLeft;
         this.xRight = mergeRect.xRight;
         this.yTop = mergeRect.yTop;
         this.yBot = mergeRect.yBot;
 
-    }
-    render(): void {
-        this.pdfBricks.forEach((pdfBrick: IPdfBrick) => {
-            pdfBrick.render();
-        });
     }
 } 
