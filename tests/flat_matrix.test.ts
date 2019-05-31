7f4103b13c8d2d9e407d425a3b8108f9d4efb01f
gologames <gologames@users.noreply.github.com>
2019-05-31 18:44:28 +0300

    Refactor tests

--- tests/flat_matrix.test.ts
+++ tests/flat_matrix.test.ts
@@ -16,18 +16,18 @@ test('test matrix hasRows true columns', async () => {
         questions: [
             {
                 titleLocation: 'hidden',
-                type: "matrix",
-                name: "test",
+                type: 'matrix',
+                name: 'test',
                 columns: [
                     {
                         value: 1,
-                        text: "test1"
+                        text: 'test1'
                     }
                 ],
                 rows: [
                     {
                         value: 1,
-                        text: "test2"
+                        text: 'test2'
                     }
                 ]
             }]
@@ -57,12 +57,12 @@ test('test matrix hasRows false columns', async () => {
         questions: [
             {
                 titleLocation: 'hidden',
-                type: "matrix",
-                name: "test",
+                type: 'matrix',
+                name: 'test',
                 columns: [
                     {
                         value: 1,
-                        text: "test1"
+                        text: 'test1'
                     }
                 ]
             }]
@@ -85,22 +85,22 @@ test('test matrix vertical', async () => {
         questions: [
             {
                 titleLocation: 'hidden',
-                type: "matrix",
-                name: "test",
+                type: 'matrix',
+                name: 'test',
                 columns: [
                     {
                         value: 1,
-                        text: "test1"
+                        text: 'test1'
                     }, {
                         value: 2,
-                        text: "test2"
+                        text: 'test2'
                     }, {
                         value: 3,
-                        text: "test3"
+                        text: 'test3'
                     },
                     {
                         value: 4,
-                        text: "test4"
+                        text: 'test4'
                     }
                 ]
             }]
@@ -124,8 +124,6 @@ test('test matrix vertical', async () => {
         currPoint = SurveyHelper.createPoint(itemRect);
     })
     TestHelper.equalRects(expect, receivedCells, assumeCells);
-
-
 });
 test('test hidden header', async () => {
     let json = {
@@ -133,19 +131,19 @@ test('test hidden header', async () => {
             {
                 titleLocation: 'hidden',
                 showHeader: false,
-                type: "matrix",
-                name: "Quality",
-                title: "Please indicate if you agree or disagree with the following statements",
+                type: 'matrix',
+                name: 'Quality',
+                title: 'Please indicate if you agree or disagree with the following statements',
                 columns: [
                     {
                         value: 1,
-                        text: "test1"
+                        text: 'test1'
                     }, {
                         value: 2,
-                        text: "test2"
+                        text: 'test2'
                     }, {
                         value: 3,
-                        text: "test3"
+                        text: 'test3'
                     }
                 ]
             }]
@@ -154,8 +152,8 @@ test('test hidden header', async () => {
     let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey);
     let assumeCells: IRect[] = [];
     let itemWidth = survey.controller.measureText().width;
-    let cellWidth = (210 * DocOptions.MM_TO_PT - survey.controller.margins.left
-        - survey.controller.margins.right) / 3;
+    let cellWidth = (210.0 * DocOptions.MM_TO_PT - survey.controller.margins.left -
+        survey.controller.margins.right) / 3;
     for (let i: number = 0; i < json.questions[0].columns.length; i++) {
         let currPoint = TestHelper.defaultPoint;
         currPoint.xLeft = cellWidth * i + survey.controller.margins.left;
@@ -169,22 +167,22 @@ test('Matrix default value', async () => {
             {
                 titleLocation: 'hidden',
                 showHeader: false,
-                type: "matrix",
-                name: "Quality",
-                title: "Please indicate if you agree or disagree with the following statements",
-                defaultValue: "Column",
+                type: 'matrix',
+                name: 'Quality',
+                title: 'Please indicate if you agree or disagree with the following statements',
+                defaultValue: 'Column',
                 columns: [
-                    "Column",
-                    "Column2"
+                    'Column',
+                    'Column2'
                 ]
             }]
     };
     let survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
     await survey.render();
     let acroFormFields = survey.controller.doc.internal.acroformPlugin.acroFormDictionaryRoot.Fields;
-    expect(acroFormFields[0].value).toBe("sq_104row0");
-    expect(acroFormFields[1].AS).toBe("/sq_104row0index0");
-    expect(acroFormFields[2].AS).toBe("/Off");
+    expect(acroFormFields[0].value).toBe('sq_104row0');
+    expect(acroFormFields[1].AS).toBe('/sq_104row0index0');
+    expect(acroFormFields[2].AS).toBe('/Off');
 });
 
 test('Matrix rubric check horisontally', async () => {
@@ -192,23 +190,23 @@ test('Matrix rubric check horisontally', async () => {
         questions: [
             {
 
-                type: "matrix",
-                titleLocation: "hidden",
-                name: "test",
+                type: 'matrix',
+                titleLocation: 'hidden',
+                name: 'test',
                 showHeader: false,
                 columns: [
-                    "column1"
+                    'column1'
                 ],
                 rows: [
                     {
-                        value: "row1",
-                        text: "row1"
+                        value: 'row1',
+                        text: 'row1'
                     }
                 ],
                 cells:
                 {
-                    "row1": {
-                        "column1": "test1"
+                    'row1': {
+                        'column1': 'test1'
                     }
 
                 }
@@ -218,8 +216,8 @@ test('Matrix rubric check horisontally', async () => {
     let survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
     let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey);
     let assumeFlats: IRect[] = [];
-    let cellWidth = (210 * DocOptions.MM_TO_PT - survey.controller.margins.left
-        - survey.controller.margins.right) / 2;
+    let cellWidth = (210.0 * DocOptions.MM_TO_PT - survey.controller.margins.left -
+        survey.controller.margins.right) / 2;
     let rowTextFlat = await SurveyHelper.createTextFlat(TestHelper.defaultPoint,
         survey.getAllQuestions()[0], survey.controller, json.questions[0].rows[0].text, TextBrick);
     let currPoint = TestHelper.defaultPoint;
@@ -227,7 +225,7 @@ test('Matrix rubric check horisontally', async () => {
     let itemFlat = SurveyHelper.createRect(currPoint, cellWidth, survey.controller.measureText(1).height);
     currPoint.yTop = itemFlat.yBot;
     let cellTextFlat = await SurveyHelper.createTextFlat(currPoint,
-        survey.getAllQuestions()[0], survey.controller, json.questions[0].cells["row1"]["column1"], TextBrick);
+        survey.getAllQuestions()[0], survey.controller, json.questions[0].cells['row1']['column1'], TextBrick);
     assumeFlats.push(rowTextFlat, itemFlat, cellTextFlat);
     TestHelper.equalRects(expect, flats[0][0].unfold(), assumeFlats);
 });
@@ -237,25 +235,25 @@ test('Matrix rubric check vertically', async () => {
         questions: [
             {
 
-                type: "matrix",
-                titleLocation: "hidden",
-                name: "test",
+                type: 'matrix',
+                titleLocation: 'hidden',
+                name: 'test',
                 showHeader: false,
                 columns: [
-                    "column1", "column2", "column3"
+                    'column1', 'column2', 'column3'
                 ],
                 rows: [
                     {
-                        value: "row1",
-                        text: "row1"
+                        value: 'row1',
+                        text: 'row1'
                     }
                 ],
                 cells:
                 {
-                    "row1": {
-                        "column1": "test1",
-                        "column2": "test2",
-                        "column3": "test3",
+                    'row1': {
+                        'column1': 'test1',
+                        'column2': 'test2',
+                        'column3': 'test3',
                     }
 
                 }
@@ -274,7 +272,7 @@ test('Matrix rubric check vertically', async () => {
         let itemFlat = SurveyHelper.createRect(currPoint, SurveyHelper.getPageAvailableWidth(survey.controller), survey.controller.measureText(1).height);
         currPoint.yTop = itemFlat.yBot;
         let cellTextFlat = await SurveyHelper.createTextFlat(currPoint,
-            survey.getAllQuestions()[0], survey.controller, (<any>json.questions[0]).cells["row1"]["column" + i], TextBrick);
+            survey.getAllQuestions()[0], survey.controller, (<any>json.questions[0]).cells['row1']['column' + i], TextBrick);
         currPoint.yTop = cellTextFlat.yBot;
         assumeFlats.push(itemFlat, cellTextFlat);
     }
