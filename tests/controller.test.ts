834d8c76ab48560bbe47b2cb8557741ad0561bac
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-06-03 11:02:36 +0300

    Add doc width and height with different orientations tests

--- tests/controller.test.ts
+++ tests/controller.test.ts
@@ -2,9 +2,9 @@
     return {};
 };
 import { DocController } from "../src/doc_controller";
-test("check font size ", () => {
+test('check font size ', () => {
     let options: any = {
-        orientation: "l",
+        orientation: 'l',
         fontSize: 12,
         margins:
         {
@@ -17,4 +17,78 @@ test("check font size ", () => {
     };
     const controller = new DocController(options);
     expect(controller.fontSize).toBe(12);
+});
+test('test doc width and heght with orinetaition \'l\' and \'a4\' format', () => {
+    let options: any = {
+        orientation: 'l',
+        fontSize: 12,
+        margins:
+        {
+            left: 10,
+            right: 10,
+            top: 10,
+            bot: 10
+
+        }
+    };
+    const controller = new DocController(options);
+    expect(controller.paperWidth).toBeCloseTo(297 * DocController.MM_TO_PT);
+    expect(controller.paperHeight).toBeCloseTo(210 * DocController.MM_TO_PT);
+});
+test('test doc width and heght with orinetaition \'l\' and array format', () => {
+    let options: any = {
+        fontSize: 12,
+        orientation: 'l',
+        paperHeight: 210,
+        paperWidth: 297,
+        margins:
+        {
+            left: 10,
+            right: 10,
+            top: 10,
+            bot: 10
+
+        }
+    };
+    const controller = new DocController(options);
+    expect(controller.paperHeight).toBeCloseTo(210 * DocController.MM_TO_PT);
+    expect(controller.paperWidth).toBeCloseTo(297 * DocController.MM_TO_PT);
+    expect(controller.orientation).toBe('l');
+});
+test('test doc width and heght change with orinetaition \'p\'', () => {
+    let options: any = {
+        fontSize: 12,
+        paperHeight: 210,
+        paperWidth: 297,
+        margins:
+        {
+            left: 10,
+            right: 10,
+            top: 10,
+            bot: 10
+
+        }
+    };
+    const controller = new DocController(options);
+    expect(controller.paperHeight).toBeCloseTo(297 * DocController.MM_TO_PT);
+    expect(controller.paperWidth).toBeCloseTo(210 * DocController.MM_TO_PT);
+});
+test('test doc width and heght change with orinetaition \'l\'', () => {
+    let options: any = {
+        fontSize: 12,
+        orientation: 'l',
+        paperHeight: 297,
+        paperWidth: 210,
+        margins:
+        {
+            left: 10,
+            right: 10,
+            top: 10,
+            bot: 10
+
+        }
+    };
+    const controller = new DocController(options);
+    expect(controller.paperHeight).toBeCloseTo(210 * DocController.MM_TO_PT);
+    expect(controller.paperWidth).toBeCloseTo(297 * DocController.MM_TO_PT);
 });
