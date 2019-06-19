0dd32965fe05b63034b17dea56a702db7558aefb
gologames <gologames@users.noreply.github.com>
2019-06-19 12:55:02 +0300

    Header and footer render stable version

--- src/event_handler/draw_canvas.ts
+++ src/event_handler/draw_canvas.ts
@@ -5,18 +5,18 @@ import { TitleBrick } from '../pdf_render/pdf_title';
 import { SurveyHelper } from '../helper_survey';
 
 export enum HorizontalAlign {
-    notset = 0,
-    left = 1,
-    center = 2,
-    right = 3,
-    justify = 4
+    NotSet = 'notset',
+    Left = 'left',
+    Center = 'center',
+    Right = 'right',
+    Justify = 'justify'
 }
 export enum VerticalAlign {
-    notset = 0,
-    top = 1,
-    middle = 2,
-    bottom = 3,
-    justify = 4
+    NotSet = 'notset',
+    Top = 'top',
+    Middle = 'middle',
+    Bottom = 'bottom',
+    Justify = 'justify'
 }
 export interface IDrawRectOptions {
     /**
@@ -38,7 +38,7 @@ export interface IDrawRectOptions {
 }
 export interface IDrawTextOptions extends IDrawRectOptions {
     /**
-     * String that will be drawn (restricted html support)
+     * String that will be drawn
      */
     text: string;
     /**
@@ -51,6 +51,14 @@ export interface IDrawTextOptions extends IDrawRectOptions {
     isBold?: boolean;
 }
 export interface IDrawImageOptions extends IDrawRectOptions {
+    /**
+     * Specifies image width (used if alignment set, canvas.rect's width by default)
+     */
+    width?: number;
+    /**
+     * Specifies image height (used if alignment set, canvas.rect's height by default)
+     */
+    height?: number;
     /**
      * String with base64 encoded image
      */
@@ -75,77 +83,89 @@ export class DrawCanvas {
     public get rect(): IRect {
         return this._rect;
     }
-    private initMargins(rectOptions: IDrawRectOptions): void {
+    private alignRect(rectOptions: IDrawRectOptions, itemSize: ISize): IRect {
         if (typeof rectOptions.margins === 'undefined') {
             rectOptions.margins = {
-                left: 0,
-                right: 0,
-                top: 0,
-                bot: 0
+                left: 0.0,
+                right: 0.0,
+                top: 0.0,
+                bot: 0.0
             }
         }
-    }
-    private alignRect(rectOptions: IDrawRectOptions, itemSize: ISize): IRect {
+        else {
+            if (typeof rectOptions.margins.left === 'undefined') {
+                rectOptions.margins.left = 0.0;
+            }
+            if (typeof rectOptions.margins.right === 'undefined') {
+                rectOptions.margins.right = 0.0;
+            }
+            if (typeof rectOptions.margins.top === 'undefined') {
+                rectOptions.margins.top = 0.0;
+            }
+            if (typeof rectOptions.margins.bot === 'undefined') {
+                rectOptions.margins.bot = 0.0;
+            }
+        }
         if (typeof rectOptions.rect === 'undefined') {
             if (typeof rectOptions.horizontalAlign === 'undefined' ||
-                rectOptions.horizontalAlign === HorizontalAlign.notset) {
-                rectOptions.horizontalAlign = HorizontalAlign.center;
+                rectOptions.horizontalAlign === HorizontalAlign.NotSet) {
+                rectOptions.horizontalAlign = HorizontalAlign.Center;
             }
             if (typeof rectOptions.verticalAlign === 'undefined' ||
-                rectOptions.verticalAlign === VerticalAlign.notset) {
-                rectOptions.verticalAlign = VerticalAlign.middle;
+                rectOptions.verticalAlign === VerticalAlign.NotSet) {
+                rectOptions.verticalAlign = VerticalAlign.Middle;
             }
         }
-        let textRect: IRect = SurveyHelper.clone(this.rect);
+        let rect: IRect = SurveyHelper.clone(this.rect);
         if (typeof rectOptions.horizontalAlign !== 'undefined') {
             switch (rectOptions.horizontalAlign) {
-                case HorizontalAlign.left:
-                    textRect.xLeft = this.rect.xLeft + rectOptions.margins.left;
-                    textRect.xRight = Math.min(this.rect.xRight - rectOptions.margins.right,
+                case HorizontalAlign.Left:
+                    rect.xLeft = this.rect.xLeft + rectOptions.margins.left;
+                    rect.xRight = Math.min(this.rect.xRight - rectOptions.margins.right,
                         this.rect.xLeft + rectOptions.margins.left + itemSize.width);
                     break;
-                case HorizontalAlign.center:
-                    textRect.xLeft = Math.max(this.rect.xLeft + rectOptions.margins.left,
-                        (this.rect.xRight - this.rect.xLeft - itemSize.width) / 2.0);
-                    textRect.xRight = Math.min(this.rect.xRight - rectOptions.margins.right,
-                        (this.rect.xRight - this.rect.xLeft + itemSize.width) / 2.0); 
+                case HorizontalAlign.Center:
+                    rect.xLeft = Math.max(this.rect.xLeft + rectOptions.margins.left,
+                        (this.rect.xLeft + this.rect.xRight - itemSize.width) / 2.0);
+                    rect.xRight = Math.min(this.rect.xRight - rectOptions.margins.right,
+                        (this.rect.xLeft + this.rect.xRight + itemSize.width) / 2.0); 
                     break;
-                case HorizontalAlign.right:
-                    textRect.xLeft = Math.max(this.rect.xLeft + rectOptions.margins.left,
+                case HorizontalAlign.Right:
+                    rect.xLeft = Math.max(this.rect.xLeft + rectOptions.margins.left,
                         this.rect.xRight - rectOptions.margins.right - itemSize.width);
-                    textRect.xRight = this.rect.xRight - rectOptions.margins.right;
+                    rect.xRight = this.rect.xRight - rectOptions.margins.right;
                     break;
             }
         }
         else {
-            textRect.xLeft = rectOptions.rect.xLeft;
-            textRect.xRight = rectOptions.rect.xRight;
+            rect.xLeft = rectOptions.rect.xLeft;
+            rect.xRight = rectOptions.rect.xRight;
         }
         if (typeof rectOptions.verticalAlign !== 'undefined') {
             switch (rectOptions.verticalAlign) {
-                case VerticalAlign.top:
-                    textRect.yTop = this.rect.yTop + rectOptions.margins.top;
-                    textRect.yBot = Math.min(this.rect.yBot - rectOptions.margins.bot,
+                case VerticalAlign.Top:
+                    rect.yTop = this.rect.yTop + rectOptions.margins.top;
+                    rect.yBot = Math.min(this.rect.yBot - rectOptions.margins.bot,
                         this.rect.yTop + rectOptions.margins.top + itemSize.height);
                     break;
-                case VerticalAlign.middle:
-                    textRect.yTop = Math.max(this.rect.yTop + rectOptions.margins.top,
-                        (this.rect.yBot - this.rect.yTop - itemSize.height) / 2.0);
-                    textRect.yBot = Math.min(this.rect.yBot - rectOptions.margins.bot,
-                        (this.rect.yBot - this.rect.yTop + itemSize.height) / 2.0); 
+                case VerticalAlign.Middle:
+                    rect.yTop = Math.max(this.rect.yTop + rectOptions.margins.top,
+                        (this.rect.yTop + this.rect.yBot - itemSize.height) / 2.0);
+                    rect.yBot = Math.min(this.rect.yBot - rectOptions.margins.bot,
+                        (this.rect.yTop + this.rect.yBot + itemSize.height) / 2.0); 
                     break;
-                case VerticalAlign.bottom:
-                    textRect.yTop = Math.max(this.rect.yTop + rectOptions.margins.top,
+                case VerticalAlign.Bottom:
+                    rect.yTop = Math.max(this.rect.yTop + rectOptions.margins.top,
                         this.rect.yBot - rectOptions.margins.bot - itemSize.height);
-                    textRect.yBot = this.rect.yBot - rectOptions.margins.bot;
+                    rect.yBot = this.rect.yBot - rectOptions.margins.bot;
                     break;
             }
         }
         else {
-            textRect.yTop = rectOptions.rect.yTop;
-            textRect.yBot = rectOptions.rect.yBot;
+            rect.yTop = rectOptions.rect.yTop;
+            rect.yBot = rectOptions.rect.yBot;
         }
-        return textRect;
+        return rect;
     }
     /**
      * Call this method to draw text 
@@ -161,7 +181,6 @@ export class DrawCanvas {
         }
         let textSize: ISize = this.controller.measureText(textOptions.text,
             textOptions.isBold ? 'bold' : 'normal', textOptions.fontSize);
-        this.initMargins(textOptions);
         let textRect: IRect = this.alignRect(textOptions, textSize);
         if (!textOptions.isBold) {
             this.packs.push(new TextBrick(null, this.controller,
@@ -177,10 +196,16 @@ export class DrawCanvas {
      * @param imageOptions 
      */
     public drawImage(imageOptions: IDrawImageOptions): void {
-        this.initMargins(imageOptions);
+        imageOptions = SurveyHelper.clone(imageOptions);
+        if (typeof imageOptions.width == 'undefined') {
+            imageOptions.width = this.rect.xRight - this.rect.xLeft;
+        }
+        if (typeof imageOptions.height == 'undefined') {
+            imageOptions.height = this.rect.yBot - this.rect.yTop;
+        }
         let imageSize: ISize = {
-            width: imageOptions.margins.right - imageOptions.margins.left,
-            height: imageOptions.margins.bot - imageOptions.margins.top
+            width: imageOptions.width,
+            height: imageOptions.height
         };
         let imageRect: IRect = this.alignRect(imageOptions, imageSize);
         this.packs.push(SurveyHelper.createImageFlat(
--- tests/event_header.test.ts
+++ tests/event_header.test.ts
@@ -3,12 +3,13 @@
 };
 
 import { SurveyPDF } from '../src/survey';
+import { ISize, IRect } from '../src/doc_controller';
 import { FlatSurvey } from '../src/flat_layout/flat_survey';
 import { FlatTextbox } from '../src/flat_layout/flat_textbox';
 import { PagePacker } from '../src/page_layout/page_packer';
 import { IPdfBrick } from '../src/pdf_render/pdf_brick';
 import { EventHandler } from '../src/event_handler/event_handler';
-import { DrawCanvas } from '../src/event_handler/draw_canvas';
+import { DrawCanvas, HorizontalAlign, VerticalAlign } from '../src/event_handler/draw_canvas';
 import { TitleBrick } from '../src/pdf_render/pdf_title';
 import { SurveyHelper } from '../src/helper_survey';
 import { TestHelper } from '../src/helper_test';
@@ -19,7 +20,7 @@ test('Event render header simple text', async () => {
         questions: [
             {
                 type: 'text',
-                name: 'event_simpletext',
+                name: 'event_headersimpletext',
                 titleLocation: 'hidden'
             }
         ]
@@ -45,7 +46,7 @@ test('Event render header bold text', async () => {
         questions: [
             {
                 type: 'text',
-                name: 'event_boldtext',
+                name: 'event_headerboldtext',
                 titleLocation: 'hidden'
             }
         ]
@@ -67,4 +68,106 @@ test('Event render header bold text', async () => {
     expect(packs[0].length).toBe(2);
     TestHelper.equalRect(expect, packs[0][1], SurveyHelper.createHeaderRect(survey.controller));
     expect(packs[0][1] instanceof TitleBrick).toBe(true);
+});
+test('Event render header left top text', async () => {
+    let json: any = {
+        questions: [
+            {
+                type: 'text',
+                name: 'event_headerlefttoptext',
+                titleLocation: 'hidden'
+            }
+        ]
+    };
+    let survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
+    let text: string = 'First on page';
+    survey.onRenderHeader.add((_, canvas: DrawCanvas) => {
+        canvas.drawText({
+            text: text,
+            horizontalAlign: HorizontalAlign.Left,
+            verticalAlign: VerticalAlign.Top
+        })
+    });
+    let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey);
+    let packs: IPdfBrick[][] = PagePacker.pack(flats, survey.controller);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(1);
+    EventHandler.process_events(survey, packs);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(2);
+    let textSize: ISize = survey.controller.measureText(text, 'normal', DrawCanvas.DEFAULT_FONT_SIZE);
+    TestHelper.equalRect(expect, packs[0][1], SurveyHelper.createRect(
+        { xLeft: 0, yTop: 0 }, textSize.width, textSize.height));
+});
+test('Event render header center middle text', async () => {
+    let json: any = {
+        questions: [
+            {
+                type: 'text',
+                name: 'event_headercentermiddleext',
+                titleLocation: 'hidden'
+            }
+        ]
+    };
+    let survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
+    let text: string = 'In the spotlight';
+    survey.onRenderHeader.add((_, canvas: DrawCanvas) => {
+        canvas.drawText({
+            text: text,
+            horizontalAlign: HorizontalAlign.Center,
+            verticalAlign: VerticalAlign.Middle
+        })
+    });
+    let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey);
+    let packs: IPdfBrick[][] = PagePacker.pack(flats, survey.controller);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(1);
+    EventHandler.process_events(survey, packs);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(2);
+    let textSize: ISize = survey.controller.measureText(text, 'normal', DrawCanvas.DEFAULT_FONT_SIZE);
+    let headerRect: IRect = SurveyHelper.createHeaderRect(survey.controller);
+    let assumeText: IRect = {
+        xLeft: (headerRect.xRight - headerRect.xLeft - textSize.width) / 2.0,
+        xRight: (headerRect.xRight - headerRect.xLeft + textSize.width) / 2.0,
+        yTop: (headerRect.yBot - headerRect.yTop - textSize.height) / 2.0,
+        yBot: (headerRect.yBot - headerRect.yTop + textSize.height) / 2.0
+    }
+    TestHelper.equalRect(expect, packs[0][1], assumeText);
+});
+test('Event render footer center middle text', async () => {
+    let json: any = {
+        questions: [
+            {
+                type: 'text',
+                name: 'event_footercentermiddletext',
+                titleLocation: 'hidden'
+            }
+        ]
+    };
+    let survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
+    let text: string = 'Exactly in the cendle';
+    survey.onRenderFooter.add((_, canvas: DrawCanvas) => {
+        canvas.drawText({
+            text: text,
+            horizontalAlign: HorizontalAlign.Center,
+            verticalAlign: VerticalAlign.Middle
+        })
+    });
+    let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey);
+    let packs: IPdfBrick[][] = PagePacker.pack(flats, survey.controller);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(1);
+    EventHandler.process_events(survey, packs);
+    expect(packs.length).toBe(1);
+    expect(packs[0].length).toBe(2);
+    let textSize: ISize = survey.controller.measureText(text, 'normal', DrawCanvas.DEFAULT_FONT_SIZE);
+    let footerRect: IRect = SurveyHelper.createFooterRect(survey.controller);
+    let assumeText: IRect = {
+        xLeft: (footerRect.xLeft + footerRect.xRight - textSize.width) / 2.0,
+        xRight: (footerRect.xLeft + footerRect.xRight + textSize.width) / 2.0,
+        yTop: (footerRect.yTop + footerRect.yBot - textSize.height) / 2.0,
+        yBot: (footerRect.yTop + footerRect.yBot + textSize.height) / 2.0
+    }
+    TestHelper.equalRect(expect, packs[0][1], assumeText);
 });
