4e9266b60578fa9f71dcbceded16b00b8b3cd39e
gologames <gologames@users.noreply.github.com>
2019-06-20 16:08:03 +0300

    Add countPages property to render header event

--- src/event_handler/event_handler.ts
+++ src/event_handler/event_handler.ts
@@ -7,9 +7,9 @@ export class EventHandler {
     public static process_events(survey: SurveyPDF, packs: IPdfBrick[][]): void {
         for (let i: number = 0; i < packs.length; i++) {
             survey.onRenderHeader.fire(this, new DrawCanvas(packs[i], survey.controller,
-                    SurveyHelper.createHeaderRect(survey.controller), i + 1));
+                    SurveyHelper.createHeaderRect(survey.controller), packs.length, i + 1));
             survey.onRenderFooter.fire(this, new DrawCanvas(packs[i], survey.controller,
-                    SurveyHelper.createFooterRect(survey.controller), i + 1));
+                    SurveyHelper.createFooterRect(survey.controller), packs.length, i + 1));
         }
     }
 }
