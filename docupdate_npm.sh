ef3da9c2b758f05ae0657c2531e5474e7909f485
gologames <gologames@users.noreply.github.com>
2019-07-04 12:31:13 +0300

    Fix doc update script

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -1,7 +1,7 @@
 mkdir __update_docs_temp
 cd ./__update_docs_temp
 mkdir service
-git clone https://$(GTTOKEN)@github.com/surveyjs/service
+git clone https://$GTTOKEN@github.com/surveyjs/service
 cp ../README.md ./Pdf-Export.md
 mv ./Pdf-Export.md ./service/surveyjs.io/App_Data/Docs
 cd ./service
