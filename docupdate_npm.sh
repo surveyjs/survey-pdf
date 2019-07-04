ffbfa879d3619bd9d4f5d078fa46edcd75317656
gologames <gologames@users.noreply.github.com>
2019-07-04 13:44:29 +0300

    Another attempt with access token

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -1,9 +1,7 @@
 mkdir __update_docs_temp
 cd ./__update_docs_temp
 mkdir service
-REPO_URL="https://github.com/surveyjs/service"
-git config --global http.$REPO_URL.extraHeader "AUTHORIZATION: Basic $GITAUTH"
-git clone $REPO_URL
+git clone https://x-access-token:$GTTOKEN@github.com/surveyjs/service
 cp ../README.md ./Pdf-Export.md
 mv ./Pdf-Export.md ./service/surveyjs.io/App_Data/Docs
 cd ./service
