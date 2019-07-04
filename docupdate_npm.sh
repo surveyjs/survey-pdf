f9e6593c240ee92df7dadf8d7376059302edc9a2
gologames <gologames@users.noreply.github.com>
2019-07-04 13:52:24 +0300

    Set name and email to git

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -1,6 +1,8 @@
 mkdir __update_docs_temp
 cd ./__update_docs_temp
 mkdir service
+git config --global user.email "gologames@bk.ru"
+git config --global user.name "gologames"
 git clone https://x-access-token:$GTTOKEN@github.com/surveyjs/service
 cp ../README.md ./Pdf-Export.md
 mv ./Pdf-Export.md ./service/surveyjs.io/App_Data/Docs
