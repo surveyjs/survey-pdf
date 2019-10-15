ff6622b8eabce32af0b9c5003fe440945bb1fde4
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-10-15 12:03:55 +0300

    Refactor boolean test

--- tests/flat_boolean.test.ts
+++ tests/flat_boolean.test.ts
@@ -1,25 +1,25 @@
-(<any>window)["HTMLCanvasElement"].prototype.getContext = async () => {
+(<any>window)['HTMLCanvasElement'].prototype.getContext = async () => {
   return {};
 };
 
-import { Question } from "survey-core";
-import { SurveyPDF } from "../src/survey";
-import { DocController } from "../src/doc_controller";
-import { FlatSurvey } from "../src/flat_layout/flat_survey";
-import { FlatQuestion } from "../src/flat_layout/flat_question";
-import { FlatBoolean } from "../src/flat_layout/flat_boolean";
-import { IPdfBrick } from "../src/pdf_render/pdf_brick";
-import { SurveyHelper } from "../src/helper_survey";
-import { TestHelper } from "../src/helper_test";
+import { Question } from 'survey-core';
+import { SurveyPDF } from '../src/survey';
+import { DocController } from '../src/doc_controller';
+import { FlatSurvey } from '../src/flat_layout/flat_survey';
+import { FlatQuestion } from '../src/flat_layout/flat_question';
+import { FlatBoolean } from '../src/flat_layout/flat_boolean';
+import { IPdfBrick } from '../src/pdf_render/pdf_brick';
+import { SurveyHelper } from '../src/helper_survey';
+import { TestHelper } from '../src/helper_test';
 let __dummy_bl = new FlatBoolean(null, null, null);
 
-test("Check new boolean", async () => {
+test('Check boolean', async () => {
   let json: any = {
     elements: [
       {
-        type: "boolean",
-        name: "Boolman",
-        title: "Ama title",
+        type: 'boolean',
+        name: 'Boolman',
+        title: 'Ama title',
         showTitle: true
       }
     ]
