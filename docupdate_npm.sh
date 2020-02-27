eef69f788eaca2c8ac82451c83df89b4376ff9ea
gologames <gologames@users.noreply.github.com>
2020-02-27 18:45:20 +0300

    Update docupdate script according to new site

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -4,10 +4,10 @@ mkdir service
 git config --global user.email "gologames@bk.ru"
 git config --global user.name "gologames"
 git clone https://x-access-token:$GTTOKEN@github.com/surveyjs/service
-cp ../README.md ./Pdf-Export.md
-mv ./Pdf-Export.md ./service/surveyjs.io/App_Data/Docs
+cp ../README.md ../docs/PdfExportOverview.md
+cp -a ../docs/. ./service/surveyjs.io/App_Data/DocsPdf
 cd ./service
-git add ./surveyjs.io/App_Data/Docs
+git add ./surveyjs.io/App_Data/DocsPdf
 git commit -m "Updated PDF documentation"
 git push
 cd ../..
