0fced766753b4ca6fa349210be989b1b31b2d52e
gologames <gologames@bk.ru>
2020-10-03 23:20:02 +0300

    Fixed empty input number text field with value 0 for T5372

--- src/pdf_render/pdf_textbox.ts
+++ src/pdf_render/pdf_textbox.ts
@@ -9,7 +9,8 @@ export class TextBoxBrick extends TextFieldBrick {
         protected isMultiline: boolean = false, index: number = 0) {
         super(question, controller, rect, isQuestion,
             (<QuestionTextModel>question).id + (isQuestion ? '' : '_comment' + index),
-            (isQuestion ? question.value : (<Question>question).comment) || '',
+            isQuestion ? (question.value !== undefined ? question.value : '') :
+                ((<Question>question).comment !== undefined ? (<Question>question).comment : ''),
             isQuestion ? SurveyHelper.getLocString((<QuestionTextModel>question).locPlaceHolder) : '',
             question.isReadOnly, isMultiline,
             (<QuestionTextModel>question).inputType);
