16e9d3c6591b60e07b1d7440c338d1b36c73c618
Yaroslav K <steep1580@yandex.ru>
2021-12-01 14:01:25 +0300

    Work for #95 - implemented empty brick border

--- src/pdf_render/pdf_empty.ts
+++ src/pdf_render/pdf_empty.ts
@@ -1,8 +1,30 @@
 import { PdfBrick } from './pdf_brick';
-import { IRect } from '../doc_controller';
+import { DocController, IRect } from '../doc_controller';
+import { SurveyHelper } from '../helper_survey';
 
 export class EmptyBrick extends PdfBrick {
-    constructor(rect: IRect) {
-        super(null, null, rect);
+    private isBorderVisible: boolean = false;
+
+    constructor(rect: IRect,
+        protected controller: DocController = null,
+        isBorderVisible: boolean = false) {
+        super(null, controller, rect);
+        this.isBorderVisible = isBorderVisible;
+    }
+
+    private resizeBorder(isIncrease: boolean): void {
+        const coef: number = isIncrease ? 1 : -1;
+        const borderPadding: number = this.controller.doc.getFontSize() * SurveyHelper.VALUE_READONLY_PADDING_SCALE;
+        this.xLeft -= coef * borderPadding;
+        this.xRight += coef * borderPadding;
+        this.yBot += coef * borderPadding;
+    }
+
+    public async renderInteractive(): Promise<void> {
+        if (this.isBorderVisible) {
+            this.resizeBorder(true);
+            SurveyHelper.renderFlatBorders(this.controller, this);
+            this.resizeBorder(false);
+        }
     }
 }
