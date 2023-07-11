8120382a0d21ef82d7fa87a78ffa42f6b5b3293b
Dmitry <dk981234@gmail.com>
2023-07-11 16:03:17 +0400

    Work for #255: support auto for image sizes (#258)

--- src/flat_layout/flat_image.ts
+++ src/flat_layout/flat_image.ts
@@ -1,6 +1,6 @@
 import { IQuestion, QuestionImageModel } from 'survey-core';
 import { SurveyPDF } from '../survey';
-import { IPoint, DocController } from '../doc_controller';
+import { IPoint, DocController, ISize } from '../doc_controller';
 import { FlatQuestion } from './flat_question';
 import { FlatRepository } from './flat_repository';
 import { IPdfBrick } from '../pdf_render/pdf_brick';
@@ -13,11 +13,35 @@ export class FlatImage extends FlatQuestion {
         super(survey, question, controller);
         this.question = <QuestionImageModel>question;
     }
+    private isSizeEmpty(val: any) {
+        return !val || val === 'auto';
+    }
+    private async getCorrectImageSize(): Promise<ISize> {
+        let widthPt: number = SurveyHelper.pxToPt(<any>this.question.imageWidth);
+        let heightPt: number = SurveyHelper.pxToPt(<any>this.question.imageHeight);
+        if(this.isSizeEmpty(this.question.imageWidth) || this.isSizeEmpty(this.question.imageHeight)) {
+            const imageSize = await SurveyHelper.getImageSize(this.question.imageLink);
+            if(!this.isSizeEmpty(this.question.imageWidth)) {
+                if(imageSize && imageSize.width) {
+                    heightPt = imageSize.height * widthPt / imageSize.width;
+                } else {
+                    heightPt = 0;
+                }
+            }
+            if(!this.isSizeEmpty(this.question.imageHeight)) {
+                if(imageSize && imageSize.height) {
+                    widthPt = imageSize.width * heightPt / imageSize.height;
+                } else {
+                    widthPt = 0;
+                }
+            }
+        }
+        return { width: widthPt, height: heightPt };
+    }
     public async generateFlatsContent(point: IPoint): Promise<IPdfBrick[]> {
-        const widthPt: number = SurveyHelper.pxToPt(<any>this.question.imageWidth);
-        const heightPt: number = SurveyHelper.pxToPt(<any>this.question.imageHeight);
+        const imageSize = await this.getCorrectImageSize();
         return [await SurveyHelper.createImageFlat(point, this.question,
-            this.controller, this.question.imageLink, widthPt, heightPt)];
+            this.controller, this.question.imageLink, imageSize.width, imageSize.height)];
     }
 }
 
--- tests/flat_image.test.ts
+++ tests/flat_image.test.ts
@@ -44,6 +44,63 @@ test('Check image question 100x100px', async () => {
     SurveyHelper.shouldConvertImageToPng = true;
 });
 
+test('Check image question with "auto"', async () => {
+    const getOldImageSize = SurveyHelper.getImageSize;
+    SurveyHelper.getImageSize = async () => {
+        return { width: 100, height: 75 };
+    };
+    SurveyHelper.shouldConvertImageToPng = false;
+    const json: any = {
+        elements: [
+            {
+                type: 'image',
+                name: 'image_question',
+                titleLocation: 'hidden',
+                imageHeight: 'auto',
+                imageWidth: 100,
+                imageLink: TestHelper.BASE64_IMAGE_100PX
+            }
+        ]
+    };
+    const survey: SurveyPDF = new SurveyPDF(json, TestHelper.defaultOptions);
+    const controller: DocController = new DocController(TestHelper.defaultOptions);
+    let flats: IPdfBrick[][] = await FlatSurvey.generateFlats(survey, controller);
+    expect(flats.length).toBe(1);
+    expect(flats[0].length).toBe(1);
+    controller.margins.left += controller.unitWidth;
+    let width: number = (<any>survey.getAllQuestions()[0]).imageWidth;
+    let widthPt: number = SurveyHelper.parseWidth(width + 'px', SurveyHelper.getPageAvailableWidth(controller));
+    let heightPt: number = 75 * widthPt / 100;
+    let assumeImage: IRect = {
+        xLeft: controller.leftTopPoint.xLeft,
+        xRight: controller.leftTopPoint.xLeft + widthPt,
+        yTop: controller.leftTopPoint.yTop,
+        yBot: controller.leftTopPoint.yTop + heightPt
+    };
+    TestHelper.equalRect(expect, flats[0][0], assumeImage);
+
+    survey.getAllQuestions()[0].imageWidth = 'auto';
+    survey.getAllQuestions()[0].imageHeight = '100';
+    flats = await FlatSurvey.generateFlats(survey, controller);
+    expect(flats.length).toBe(1);
+    expect(flats[0].length).toBe(1);
+    controller.margins.left += controller.unitWidth;
+    let height = (<any>survey.getAllQuestions()[0]).imageHeight;
+    heightPt = SurveyHelper.parseWidth(width + 'px', SurveyHelper.getPageAvailableWidth(controller));
+    widthPt = 100 * heightPt / 75;
+
+    assumeImage = {
+        xLeft: controller.leftTopPoint.xLeft,
+        xRight: controller.leftTopPoint.xLeft + widthPt,
+        yTop: controller.leftTopPoint.yTop,
+        yBot: controller.leftTopPoint.yTop + heightPt
+    };
+    TestHelper.equalRect(expect, flats[0][0], assumeImage);
+
+    SurveyHelper.shouldConvertImageToPng = true;
+    SurveyHelper.getImageSize = getOldImageSize;
+});
+
 test('Check image question 100x100px with set size server-side', async () => {
     SurveyHelper.inBrowser = false;
     const json: any = {
