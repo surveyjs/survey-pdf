2c7fa483f2b44dd9d4b47961bfc83c64bbbfa039
Aleksey Novikov <novikov82@gmail.com>
2025-05-12 18:13:01 +0300

    #388 - fix platform json

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,10 @@ const buildPlatformJson = {
       "import": "./fesm/survey.pdf.fonts.mjs",
       "require": "./survey.pdf.fonts.js"
     },
+    "./survey.pdf.forms": {
+      "import": "./fesm/survey.pdf.forms.mjs",
+      "require": "./survey.pdf.forms.js"
+    },
   }
 }
 
