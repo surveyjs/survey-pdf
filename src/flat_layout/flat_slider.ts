7847a8750d738ea7702c936bd2e5a01b5a51b540
dmitrykurmanov <kurmanov.work@gmail.com>
2025-06-23 21:22:38 +0400

    work for the https://github.com/surveyjs/survey-pdf/issues/414

--- src/flat_layout/flat_slider.ts
+++ src/flat_layout/flat_slider.ts
@@ -1,4 +1,4 @@
-import { IQuestion, QuestionSliderModel, QuestionTextModel } from 'survey-core';
+import { IQuestion, QuestionSliderModel, QuestionTextModel, settings } from 'survey-core';
 import { SurveyPDF } from '../survey';
 import { IPoint, IRect, DocController } from '../doc_controller';
 import { FlatQuestion } from './flat_question';
@@ -7,6 +7,7 @@ import { IPdfBrick } from '../pdf_render/pdf_brick';
 import { TextBoxBrick } from '../pdf_render/pdf_textbox';
 import { SurveyHelper } from '../helper_survey';
 import { FlatTextbox } from './flat_textbox';
+import { ITextFieldBrickOptions, TextFieldBrick } from 'src/pdf_render/pdf_textfield';
 
 export class FlatSlider extends FlatQuestion {
     protected question: QuestionSliderModel;
@@ -17,13 +18,23 @@ export class FlatSlider extends FlatQuestion {
     }
     public async generateFlatsContent(point: IPoint): Promise<IPdfBrick[]> {
         if (this.question.sliderType === 'single') {
-            const rect: IRect = SurveyHelper.createTextFieldRect(point, this.controller);
-            return [new TextBoxBrick(this.question, this.controller, rect)];
+            const options: Omit<ITextFieldBrickOptions, 'isMultiline'> = {
+                fieldName: this.question.id,
+                inputType: this.question.inputType,
+                value: this.question.inputValue,
+                isReadOnly: this.question.isReadOnly,
+                shouldRenderBorders: settings.readOnlyTextRenderMode === 'input',
+                placeholder: SurveyHelper.getLocString(this.question.locPlaceHolder)
+            };
+            if (!this.shouldRenderAsComment) {
+                const rect: IRect = SurveyHelper.createTextFieldRect(point, this.controller);
+                return [new TextFieldBrick(this.question, this.controller, rect, { ...options })];
+            }
         }
 
-        if (this.question.sliderType === 'range') {
-            return;
-        }
+        // if (this.question.sliderType === 'range') {
+        //     return;
+        // }
     }
 }
 
