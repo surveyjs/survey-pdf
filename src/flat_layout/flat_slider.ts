592cec1a5b15b5ab81b79b6d8042b577503780cb
dmitrykurmanov <kurmanov.work@gmail.com>
2025-06-23 21:29:27 +0400

    work for the https://github.com/surveyjs/survey-pdf/issues/414

--- src/flat_layout/flat_slider.ts
+++ src/flat_layout/flat_slider.ts
@@ -4,10 +4,8 @@ import { IPoint, IRect, DocController } from '../doc_controller';
 import { FlatQuestion } from './flat_question';
 import { FlatRepository } from './flat_repository';
 import { IPdfBrick } from '../pdf_render/pdf_brick';
-import { TextBoxBrick } from '../pdf_render/pdf_textbox';
 import { SurveyHelper } from '../helper_survey';
-import { FlatTextbox } from './flat_textbox';
-import { ITextFieldBrickOptions, TextFieldBrick } from 'src/pdf_render/pdf_textfield';
+import { ITextFieldBrickOptions, TextFieldBrick } from '../pdf_render/pdf_textfield';
 
 export class FlatSlider extends FlatQuestion {
     protected question: QuestionSliderModel;
@@ -20,11 +18,10 @@ export class FlatSlider extends FlatQuestion {
         if (this.question.sliderType === 'single') {
             const options: Omit<ITextFieldBrickOptions, 'isMultiline'> = {
                 fieldName: this.question.id,
-                inputType: this.question.inputType,
-                value: this.question.inputValue,
+                inputType: 'number',
+                value: this.question.value,
                 isReadOnly: this.question.isReadOnly,
-                shouldRenderBorders: settings.readOnlyTextRenderMode === 'input',
-                placeholder: SurveyHelper.getLocString(this.question.locPlaceHolder)
+                shouldRenderBorders: true,
             };
             if (!this.shouldRenderAsComment) {
                 const rect: IRect = SurveyHelper.createTextFieldRect(point, this.controller);
