084b1e6c8a5ab490887826d3188d2b0fd51449a7
Dmitry Kuzin <dk981234@gmail.com>
2025-08-27 12:29:15 +0400

    Fix tests

--- setupFile.js
+++ setupFile.js
@@ -1,1 +1,10 @@
+const  { TextEncoder, TextDecoder } = require("util");
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+global.atob = function (str) {
+  return Buffer.from(str, 'base64').toString('binary');
+}
+global.btoa = function (str) {
+  return Buffer.from(str, 'binary').toString('base64');
+}
 global.queueMicrotask = (cb) => { cb(); }
