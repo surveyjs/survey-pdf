d53c73b7a78edd8b8429aa628375fe3677691d09
Dmitry Kuzin <dk981234@gmail.com>
2025-12-16 10:09:12 +0400

    Fix base64 images are empty on node

--- src/utils/image/node.ts
+++ src/utils/image/node.ts
@@ -3,9 +3,14 @@ import fetch from 'node-fetch';
 import { imageSize } from 'image-size';
 export class ImageUtils extends BaseImageUtils implements IImageUtils {
     protected async _getImageInfo(url: string): Promise<IImageInfo> {
-        const res = await fetch(url);
         const pxToPt: number = 72.0 / 96.0;
-        const data = new Uint8Array(await res.arrayBuffer());
+        let data: Uint8Array;
+        if (typeof url === 'string' && url.startsWith('data:')) {
+            data = Buffer.from(url.split(',')[1] || '', 'base64');
+        } else {
+            const res = await fetch(url);
+            data = new Uint8Array(await res.arrayBuffer());
+        }
         const size = imageSize(data);
         return { data, width: size.width * pxToPt, height: size.height * pxToPt, id: this.getImageId() };
     }
