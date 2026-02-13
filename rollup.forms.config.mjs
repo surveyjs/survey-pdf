5098aae0a16ac24ab904f292b53a6e4f64164a23
Dmitry <dk981234@gmail.com>
2026-02-13 17:53:57 +0700

    Use rollup instead of webpack to build umd modules (#459)
    * Use rollup instead of webpack
    
    * Update rollup helpers
    
    * Fix node build
    
    * Pass version to helpers

---
+++ rollup.forms.config.mjs
@@ -0,0 +1,52 @@
+import { createEsmConfig, createUmdConfig } from './rollup.helpers.mjs';
+import { resolve } from 'node:path';
+import { env } from 'node:process';
+import { fileURLToPath, URL } from 'node:url';
+import packageJSON from './package.json' assert { type: "json" };
+const version = packageJSON.version;
+const buildPath = fileURLToPath(new URL('./build', import.meta.url));
+const external = {
+    'fs': 'fs'
+};
+export default [
+    createEsmConfig({
+        sharedFileName: 'pdf-form-filler-shared.mjs',
+        tsconfig: fileURLToPath(new URL('./tsconfig.forms.json', import.meta.url)),
+        external,
+        dir: resolve(buildPath, './fesm'),
+        input: {
+            'pdf-form-filler': fileURLToPath(new URL('./src/entries/forms.ts', import.meta.url)),
+            'pdf-form-filler.node': fileURLToPath(new URL('./src/entries/forms-node.ts', import.meta.url))
+        },
+        version
+    }),
+    createUmdConfig({
+        tsconfig: fileURLToPath(new URL('./tsconfig.forms.json', import.meta.url)),
+        external,
+        declarationDir: resolve(buildPath, './forms-typings'),
+        dir: resolve(buildPath),
+        emitMinified: env.emitMinified === 'true',
+        globalName: 'PDFFormFiller',
+        globals: {
+            'fs': 'fs'
+        },
+        input: {
+            'pdf-form-filler': fileURLToPath(new URL('./src/entries/forms.ts', import.meta.url)),
+        },
+        version
+    }),
+    createUmdConfig({
+        tsconfig: fileURLToPath(new URL('./tsconfig.forms.json', import.meta.url)),
+        external,
+        dir: buildPath,
+        emitMinified: env.emitMinified === 'true',
+        globalName: 'PDFFormFiller',
+        globals: {
+            'fs': 'fs'
+        },
+        input: {
+            'pdf-form-filler.node': fileURLToPath(new URL('./src/entries/forms-node.ts', import.meta.url)),
+        },
+        version
+    })
+];
