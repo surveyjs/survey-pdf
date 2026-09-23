f76aefcf3b2acad8361199401c1b2828970b019a
Dmitry <dk981234@gmail.com>
2026-09-23 10:24:45 +0200

    Fix radio causes memory leak (#573)

--- src/jspdf_plugins/acroform_radio.ts
+++ src/jspdf_plugins/acroform_radio.ts
@@ -1,11 +1,24 @@
-export default function setRadioAppearance(doc: any): any {
-    const oldAppearanceFuncition: Function = doc.AcroFormAppearance.RadioButton.Circle.YesNormal;
-    doc.AcroFormAppearance.RadioButton.Circle.YesNormal = function(formObject: any) {
-        const xobj: any = oldAppearanceFuncition(formObject);
-        const stream: string[] = xobj.stream.split('\n');
-        const encodeColor: string = doc.__private__.encodeColorString(formObject.color);
-        stream[0] = stream[0] + '\n' + encodeColor + '\n' + encodeColor.toUpperCase();
-        xobj.stream = stream.join('\n');
-        return xobj;
+export function getPatchedAcroFormRadioButton(doc: any) {
+    return class extends doc.AcroFormRadioButton {
+        constructor() {
+            super();
+        }
+        setAppearance(appearance: any) {
+            super.setAppearance(appearance);
+            const oldAppearanceFuncition = appearance.YesNormal;
+            for (const objId in this.Kids) {
+                if (this.Kids.hasOwnProperty(objId)) {
+                    const child = this.Kids[objId];
+                    child.appearanceStreamContent.N[child.optionName] = function(formObject: any) {
+                        const xobj: any = oldAppearanceFuncition(formObject);
+                        const stream: string[] = xobj.stream.split('\n');
+                        const encodeColor: string = doc.__private__.encodeColorString(formObject.color);
+                        stream[0] = stream[0] + '\n' + encodeColor + '\n' + encodeColor.toUpperCase();
+                        xobj.stream = stream.join('\n');
+                        return xobj;
+                    };
+                }
+            }
+        }
     };
 }
